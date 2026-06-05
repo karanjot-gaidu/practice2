e700a0a4f81034e2e3a7e5e7e04070af741585ee
Karanjot Gaidu <90838376+karanjot-gaidu@users.noreply.github.com>
2026-06-05 02:18:13 -0400

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u].append((v, w))
+        
+        inf = float('inf')
+        dist = {node: inf for node in range(1, n+1)}
+
+        q = deque([(k, 0)])
+        dist[k] = 0
+
+        while q:
+            node, time = q.popleft()
+            if dist[node] < time:
+                continue
+            for nei, w in adj[node]:
+                if time + w < dist[nei]:
+                    dist[nei] = time + w
+                    q.append((nei, time + w))
+        
+        res = max(dist.values())
+        return res if res < inf else -1
